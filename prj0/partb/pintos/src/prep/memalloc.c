a1abed7bc2c0dc7d746cc189528418a2bd0063a5
Ranmocy Sheng <ranmocy@gmail.com>
2014-08-28 11:09:01 -0700

    remove some debug output

--- prj0/partb/pintos/src/prep/memalloc.c
+++ prj0/partb/pintos/src/prep/memalloc.c
@@ -29,70 +29,54 @@ void mem_init(uint8_t *base, size_t length)
 {
     g_base = base; // DEBUG:
     printf("\nDEBUG: BASE: %d -> %d, LENGTH: %d\n", (int)base, (int)(base + length), (int)length);
-    // printf("DEBUG: list: %d, head: %d, tail: %d\n", p_addr(&free_block_list), p_addr(&(free_block_list.head)), p_addr(&(free_block_list.tail)));
-    // printf("DEBUG: list: size: %d\n", (int)sizeof(struct list));
 
     list_init(&free_block_list);
 
     struct free_block *block_p = (struct free_block *) base;
     block_p->length = length;
     list_push_front(&free_block_list, &(block_p->elem));
-    mem_dump_free_list();
+    mem_dump_free_list(); // DEBUG:
 }
 
 /* Allocate 'length' bytes of memory. */
 void * mem_alloc(size_t length)
 {
-    printf(KRED "\nDEBUG: MALLOC: %d\n" RESET, (int)length);
-    mem_dump_free_list(); // DEBUG:
-
     size_t length_needed = sizeof(struct used_block) + length;
-    // printf("DEBUG: search: %d blocks for length %d\n", (int)list_size(&free_block_list), (int)length_needed);
+    printf(KRED "\nDEBUG: MALLOC: %d, real: %d\n" RESET, (int)length, (int)length_needed);
+    mem_dump_free_list(); // DEBUG:
 
     for (struct list_elem *e = list_begin(&free_block_list);
          e != list_end(&free_block_list);
          e = list_next(e))
     {
         struct free_block *f = list_entry(e, struct free_block, elem);
-        // printf("  DEBUG: free_block: %d\n", p_addr(f));
 
         // if we have more space
         if (f->length > length_needed + sizeof(struct free_block)) {
             struct free_block *n = (struct free_block *)((void *)f + length_needed);
-            // printf("  DEBUG: %d + %d = %d\n", p_addr(f), (int)length_needed, p_addr(n));
             n->length = f->length - length_needed;
-            // printf("  DEBUG: next_block: %d, length: %d\n", p_addr(n), (int)(n->length));
-            printf("  DEBUG: before_insert: %d, %d\n", p_addr(list_next(e)), p_addr(&(n->elem)));
-
-                // mem_dump_free_list(); // DEBUG:
             list_insert(list_next(e), &(n->elem));
         }
 
         // or if we can use the whole block
         if (f->length >= length_needed) {
-                // mem_dump_free_list(); // DEBUG:
-            // printf("  DEBUG: removing: &%d -> (&%d) -> &%d\n", p_addr(list_prev(e)), p_addr(e), p_addr(list_next(e)));
             list_remove(e);
-                // mem_dump_free_list(); // DEBUG:
 
             struct used_block *u = (struct used_block *) f;
             u->length = length;
 
-            // printf("  DEBUG: ptr: %d\n", (int)&(u->data));
             mem_dump_free_list(); // DEBUG:
-
             return &(u->data);
         }
     }
 
-    printf("DEBUG: MALLOC FAILED!\n");
     return NULL;
 }
 
 // Union two free_block CURRENT with the next free_block
 void union_block(struct free_block *current) {
     struct free_block *next = next_free_block(current);
-    printf("\nDEBUG: UNION_BLOCK: %d with %d\n", p_addr(current), p_addr(next));
+    printf(KYEL "\nDEBUG: UNION_BLOCK: %d with %d\n" RESET, p_addr(current), p_addr(next));
     current->length += next->length;
     list_remove(&(next->elem));
 }
@@ -104,9 +88,7 @@ void mem_free(void *ptr)
     mem_dump_free_list(); // DEBUG:
 
     struct used_block *u = (struct used_block *)(ptr - sizeof(struct used_block));
-    // printf("DEBUG: %d - %d = %d\n", p_addr(ptr), (int)sizeof(struct used_block), p_addr(u));
     size_t block_length = u->length + sizeof(struct used_block);
-    // printf("DEBUG: block from %d, length %d\n", p_addr(u), (int)block_length);
 
     struct free_block *next_free_block = get_free_block(list_end(&free_block_list));
 
@@ -115,24 +97,19 @@ void mem_free(void *ptr)
          e = list_next(e))
     {
         struct free_block *current = list_entry(e, struct free_block, elem);
-        // printf("  DEBUG: block: (%d) <=> %d\n", p_addr(u), p_addr(current));
 
         if ((uintptr_t)u < (uintptr_t)current) {
             next_free_block = current;
             break;
         }
     }
-    // printf("DEBUG: got_block: %d\n", p_addr(next_free_block));
 
     struct free_block *f = (struct free_block *) u;
     f->length = block_length;
-    // printf("DEBUG: free_block: %d, length %d\n", p_addr(f), (int)(f->length));
     block_insert(next_free_block, f);
-        // mem_dump_free_list(); // DEBUG:
 
     // if we need to union before
     struct free_block *prev = block_prev(f);
-    // printf("DEBUG: union_prev: %d + %d == %d\n", p_addr(prev), (int)(prev->length), p_addr(f));
     if ((uintptr_t)prev + prev->length == (uintptr_t)f) {
         union_block(prev);
         f = prev;
@@ -140,9 +117,7 @@ void mem_free(void *ptr)
 
     // if we need to union after
     struct free_block *next = block_next(f);
-    // printf("DEBUG: union_next: %d + %d == %d\n", p_addr(f), (int)(f->length), p_addr(next));
     if ((uintptr_t)f + f->length == (uintptr_t)next) {
-        // printf("DEBUG: \n");
         union_block(f);
     }
 
@@ -152,7 +127,6 @@ void mem_free(void *ptr)
 /* Return the number of elements in the free list. */
 size_t mem_sizeof_free_list(void)
 {
-    printf("\nDEBUG: LIST_SIZE: %d\n", (int)list_size(&free_block_list));
     return list_size(&free_block_list);
 }
 
