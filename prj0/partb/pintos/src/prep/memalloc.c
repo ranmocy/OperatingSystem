d360488ae013b8365f914d22af579c39f18bcbd7
Ranmocy Sheng <ranmocy@gmail.com>
2014-08-28 19:52:42 -0700

    remove debug output and rewrite mem_dump_free_list

--- prj0/partb/pintos/src/prep/memalloc.c
+++ prj0/partb/pintos/src/prep/memalloc.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
 #include "memalloc.h"
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
-#define RESET "\033[0m"
-
-#define addr_d(PTR) (int)((void *)PTR - (void *)g_base)
-#define debug(STR, ...) printf(KYEL "\nDEBUG: " STR "\n" RESET, __VA_ARGS__)
-
+#define addr_d(PTR)                 (int)((void *)PTR - (void *)g_base)
 #define ASSERT_PTR(PTR)             ASSERT((void *)g_base <= (void *)PTR || !"PTR underflow!") && \
                                     ASSERT((void *)PTR < (void *)g_bound || !"PTR overflow!")
 
@@ -21,6 +9,7 @@
 #define get_used_block(PTR)         (struct used_block *)(PTR - sizeof(struct used_block))
 #define block_begin()               get_free_block(list_begin(&free_block_list))
 #define block_end()                 get_free_block(list_end(&free_block_list))
+#define block_rend()                get_free_block(list_rend(&free_block_list))
 #define block_next(FB_P)            get_free_block(list_next(&(FB_P->elem)))
 #define block_prev(FB_P)            get_free_block(list_prev(&(FB_P->elem)))
 #define block_insert(FB1_P, FB2_P)  list_insert(&(FB1_P->elem), &(FB2_P->elem))
@@ -48,7 +37,6 @@ void block_slice(struct free_block *f, size_t length)
 void union_block(struct free_block *current)
 {
     struct free_block *next = block_next(current);
-    debug("UNION_BLOCK: %d with %d", addr_d(current), addr_d(next));
     current->length += next->length;
     list_remove(&(next->elem));
 }
@@ -67,17 +55,13 @@ void union_block_if_adjacent(struct free_block *current)
 // Initialize memory allocator to use LENGTH bytes of memory at BASE
 void mem_init(uint8_t *base, size_t length)
 {
-    g_base = base; // DEBUG:
-    g_bound = base + length; // DEBUG:
-    debug("BASE: %d -> %d, LENGTH: %d", (int)g_base, (int)g_bound, (int)length);
-
+    g_base = base;
+    g_bound = base + length;
     list_init(&free_block_list);
 
     struct free_block *f = (struct free_block *) base;
     f->length = length;
     block_push_front(f);
-
-    mem_dump_free_list(); // DEBUG:
 }
 
 
@@ -87,7 +71,6 @@ void * mem_alloc(size_t length)
     if (length == 0) return NULL;
 
     size_t length_needed = sizeof(struct used_block) + length;
-    debug(KRED "MALLOC: " RESET "%d, real: %d", (int)length, (int)length_needed);
 
     struct free_block *f;
     for (f = block_begin(); f != block_end(); f = block_next(f)) {
@@ -110,7 +93,6 @@ void * mem_alloc(size_t length)
                 block_insert(next, n);
             }
 
-            mem_dump_free_list(); // DEBUG:
             return &(u->data);
         }
     }
@@ -121,7 +103,6 @@ void * mem_alloc(size_t length)
 // Free memory pointed to by PTR
 void mem_free(void *ptr)
 {
-    debug(KGRN "FREE: " RESET "%d", addr_d(ptr));
     struct used_block *u = get_used_block(ptr);
     struct free_block *next_free_block = block_end(); // Default
     struct free_block *current;
@@ -139,8 +120,6 @@ void mem_free(void *ptr)
     // union if adjacent
     union_block_if_adjacent(f);
     union_block_if_adjacent(block_prev(f));
-
-    mem_dump_free_list(); // DEBUG:
 }
 
 // Return the number of elements in the free list.
@@ -150,18 +129,12 @@ size_t mem_sizeof_free_list()
 }
 
 // Dump the free list.  Implementation of this method is optional.
+#define DUMP_BLOCK(FB_P, NAME) printf("\t" NAME ":\t%d -> %d: length %d\n", addr_d(FB_P), addr_d(FB_P + FB_P->length), (int)(FB_P->length));
 void mem_dump_free_list()
 {
-    struct free_block *f;
-    f = list_entry(list_rend(&free_block_list), struct free_block, elem);
-    printf("\thead:\t%d -> %d: length %d\n", addr_d(f), addr_d(f + f->length), (int)(f->length));
-    for (struct list_elem *e = list_begin(&free_block_list);
-         e != list_end(&free_block_list);
-         e = list_next(e))
-    {
-        f = list_entry(e, struct free_block, elem);
-        printf("\tblock:\t%d -> %d: length %d\n", addr_d(f), addr_d(f + f->length - 1), (int)(f->length));
-    }
-    f = list_entry(list_end(&free_block_list), struct free_block, elem);
-    printf("\ttail:\t%d -> %d: length %d\n\n", addr_d(f), addr_d(f + f->length), (int)(f->length));
+    DUMP_BLOCK(block_rend(), "head");
+    for (struct free_block *f = block_begin(); f != block_end(); f = block_next(f))
+        DUMP_BLOCK(f, "block");
+    DUMP_BLOCK(block_end(), "tail");
 }
+#undef DUMP_BLOCK
