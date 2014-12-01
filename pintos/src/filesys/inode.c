e8d2c88d03926735cfd364cf958db94b8e5c3146
yaxum62 <yaxum62@gmail.com>
2014-12-01 12:58:36 -0800

    b_tree_complete

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -7,339 +7,356 @@
 #include "filesys/free-map.h"
 #include "threads/malloc.h"
 
-/* Identifies an inode. */
+#define NULL_SECTOR -1
+
 #define INODE_MAGIC 0x494e4f44
+#define TREE_ORDER ((BLOCK_SECTOR_SIZE - 4 - 2*sizeof(block_sector_t) - sizeof(unsigned))/sizeof(struct b_tree_record))
+#define LAST_ELEM (TREE_ORDER - 1)
+struct b_tree_record{
+	off_t off;
+	block_sector_t sector;
+};
+struct b_tree_node{
+	uint16_t height;
+	int16_t nkey;
+	block_sector_t next;
+	block_sector_t parent;		// for root node, this is always NULL_SECTOR
+	unsigned magic;
+	struct b_tree_record data[TREE_ORDER];
+} ;
 
-/* On-disk inode.
-   Must be exactly BLOCK_SECTOR_SIZE bytes long. */
-struct inode_disk
-  {
-    block_sector_t start;               /* First data sector. */
-    off_t length;                       /* File size in bytes. */
-    unsigned magic;                     /* Magic number. */
-    uint32_t unused[125];               /* Not used. */
-  };
-
-/* Returns the number of sectors to allocate for an inode SIZE
-   bytes long. */
-static inline size_t
-bytes_to_sectors (off_t size)
-{
-  return DIV_ROUND_UP (size, BLOCK_SECTOR_SIZE);
+/* returns the index of the last element which <= off , or -1 if no such an element exists.*/
+int16_t b_tree_search_node(struct b_tree_node* ptr, off_t off){
+	int min, max, mid;
+	min = 0; max = ptr->nkey;
+	if (min == max || ptr->data[0].off > off)
+		return -1;
+	while (true){
+		if (ptr->data[min].off == off || min + 1 == max)
+			return min;
+		mid = (min + max) / 2;
+		if (ptr->data[mid].off > off)
+			max = mid;
+		else
+			min = mid;
+	}
 }
 
-/* In-memory inode. */
+block_sector_t b_tree_find(block_sector_t root, off_t off){
+	struct b_tree_node* ptr;
+
+	ptr = (struct b_tree_node*)cache_get_ptr_ro(root);
+	if (ptr->data[0].off > off)
+		return NULL_SECTOR;
+	while (true){
+		int16_t t = b_tree_search_node(ptr, off);
+		ASSERT(t >= 0);
+		if (!ptr->height)
+			return (ptr->data[t].off == off)?ptr->data[t].sector:NULL_SECTOR;
+		root = ptr->data[t].sector;
+		cache_release_ptr(ptr);
+		ptr = (struct b_tree_node*)cache_get_ptr_ro(root);
+	}
+}
+
+void b_tree_update_first(struct b_tree_node* ptr, off_t new_off){
+	off_t old_off = ptr->data[0].off
+	block_sector_t sector;
+
+	ptr->data[0].off = new_off;
+	sector = ptr->parent;
+	while (sector != NULL_SECTOR){
+		int16_t t;
+		ptr = (struct b_tree_node*) cache_get_ptr_rw(sector);
+		t = b_tree_search_node(ptr, old_off);
+		ASSERT(t >= 0 && ptr->data[t].off == old_off);
+		ptr->data[t].off = new_off;
+		sector = ptr->parent;
+		cache_release_ptr(ptr);
+		if (t) break;
+	}
+}
+
+enum insert_result{
+	INS_SUCCESS,
+	INS_FAIL,
+	INS_FATAL
+}
+
+void shift_right(struct b_tree_node* ptr, int16_t loc, off_t off, block_sector_t sector){
+	if (loc == 0){
+		memmove(ptr->data + 1, ptr->data, min(LAST_ELEM, ptr->nkey)  * sizeof(struct b_tree_record));
+		b_tree_update_first(ptr, off);
+		ptr->data[0].sector = sector;
+	}else{
+		memmove(ptr->data + loc + 1, ptr->data + loc, (min(LAST_ELEM, ptr->nkey) - loc) * sizeof(struct b_tree_record))
+		ptr->data[loc].off = off;
+		ptr->data[loc].sector = sector;
+	}
+}
+
+inline void set_parent(block_sector_t parent, block_sector_t child){
+	struct b_tree_node* childptr = (struct b_tree_node*)cache_get_ptr_rw(child);
+	childptr->parent = parent;
+	cache_release_ptr(childptr);
+}
+
+inline void move_child(struct b_tree_block* src,
+						int16_t src_off,
+						struct b_tree_block* dst,
+						block_sector_t dst_sector.
+						int16_t dst_off,
+						int16_t size){
+	int i;
+	ASSERT(src->height == dst->height);
+	if (src != dst && src->height)
+		for (i = 0; i < size; i++)
+			set_parent(dst_sector, src->data[src_off + i].sector);
+	memmove(dst->data+dst_off, src->data+src_off, size * sizeof(struct b_tree_record));
+}
+
+bool b_tree_insert(struct b_tree_node* ptr, off_t off, block_sector_t sector, block_sector_t root){
+	int16_t t = b_tree_search_node(ptr, off);
+	struct b_tree_node * next = NULL;
+
+	ASSERT(t < 0 || ptr->offs[t] != off);
+
+	if (ptr->nkey < TREE_ORDER){
+		shift_right(ptr, t + 1, off, sector);
+		ptr->nkey++;
+		return true;
+	}else if (ptr->parent == NULL_SECTOR){
+		// It's root node
+		bool res;
+		block_sector_t a,b;
+		struct b_tree_node *aptr, *bptr;
+		int16_t sep = TREE_ORDER / 2;
+		if (!free_map_allocate(1, &a))
+			return false;
+		if (!free_map_allocate(1, &b)){
+			free_map_release(a, 1);
+			return false;
+		}
+		aptr = (struct b_tree_node*)cache_get_ptr_rw(a);
+		bptr = (struct b_tree_node*)cache_get_ptr_rw(b);
+
+		aptr->height = bptr->height = ptr->height;
+		aptr->nkey = sep;
+		bptr->nkey = TREE_ORDER - sep;
+		aptr->next = b;
+		bptr->next = NULL_SECTOR;
+		aptr->parent = bptr->parent = root;
+		move_child(ptr, 0, aptr, a, 0, aptr->nkey);
+		move_child(ptr, sep, bptr, b, 0, bptr->nkey);
+
+		ptr->height++;
+		ptr->nkey = 2;
+		ptr->data[0].off = aptr->data[0].off;
+		ptr->data[0].sector = a;
+		ptr->data[1].off = bptr->data[0].off;
+		ptr->data[1].sector = b;
+		
+		// TODO res =  retry in aptr and bptr
+		cache_release_ptr(bptr);
+		cache_release_ptr(aptr);
+		return res;
+	}else if(ptr->next == NULL_SECTOR){
+		// if next node is not exist.
+		bool res;
+		struct b_tree_node *next, *par;
+		block_sector_t next_sector;
+		int16_t sep = TREE_ORDER / 2;
+		if (!free_map_allocate(1, &next_sector)
+			return false;
+		next = (struct b_tree_node*)cache_get_ptr_rw(ptr->next);
+		next->height = ptr->height;
+		next->nkey = TREE_ORDER - sep;
+		next->next = NULL_SECTOR;
+		next->parent = ptr->parent;
+
+		par = (struct b_tree_node*)cache_get_ptr_rw(ptr->parent);
+		move_child(ptr, sep, next, ptr->next, 0, next->nkey);
+		if (!b_tree_insert(par, next->data[0].off, ptr->next, root)){
+			cache_release_ptr(par);
+			cache_release_ptr(next);
+			free_map_release(next_sector);
+			return false;
+		}
+		ptr->nkey = sep;
+		ptr->next = next_sector;
+		// TODO res = retry in ptr and next
+		cache_release_ptr(par);
+		cache_release_ptr(next);
+		return res;
+	}else if ((next=(struct b_tree_node*) cache_get_ptr_rw(ptr->next))->nkey < TREE_ORDER){
+		// if exist next node and it's not full.
+		if (loc == TREE_ORDER){
+			shift_right(next, 0, off, record);
+			if (ptr->height)
+				set_parent(ptr->next, sector);
+		}else{
+			shift_right(next, 0, ptr->data[LAST_ELEM].off, ptr->data[LAST_ELEM].sector);
+			next->data[0] = ptr->data[LAST_ELEM];
+			if (ptr->height)
+				set_parent(ptr->next, next->data[0].sector)
+			shift_right(ptr, loc, off, sector);
+		}
+		next->nkey++;
+		cache_release_ptr(next);
+		return true;
+	}else{
+		// both this and next node is full.
+		bool res;
+		off_t old_off;
+		struct b_tree_node *mid, par;
+		block_sector_t mid_sector;
+		int16_t sep1 = TREE_ORDER *2 / 3, sep2 = TREE_ORDER * 4 / 3;
+		if (!free_map_allocate(1, &mid_sector))
+			return false;
+		mid = (struct b_tree_node*)cache_get_ptr_rw(mid_sector);
+		mid->height = ptr->height;
+		mid->nkey = sep2 - sep1;
+		mid->next = ptr->next;
+		mid->parent = ptr->parent;
+
+		move_child(ptr, sep1, mid, mid_sector, 0,TREE_ORDER-sep1);
+		move_child(next, 0, mid, mid_sector, TREE_ORDER-sep1, sep2 - TREE_ORDER);
+		par = (struct b_tree_node*)cache_get_ptr_rw(ptr->parent);
+		if (!b_tree_insert(par, mid->data[0].off, mid_sector, root)){
+			cache_release_ptr(par);
+			cache_release_ptr(mid);
+			cache_release_ptr(next);
+			free_map_allocate(mid_sector, 1);
+			return false;
+		}
+		ptr->nkey = sep1;
+		ptr->next = mid_sector;
+
+		next->nkey =  2*TREE_ORDER - sep2;
+		next->data[0].off;
+		b_tree_update_first(next, next->data[sep2-TREE_ORDER].off);
+		move_child(next, sep2-TREE_ORDER, next, mid->next, 0, next->nkey);
+		// TODO res = retry in ptr, mid and next
+		cache_release_ptr(par);
+		cache_release_ptr(mid);
+		cache_release_ptr(next);
+		return res;
+	}
+}
+
+void b_tree_remove(block_sector_t sector){
+	struct b_tree_node* ptr = (struct b_tree_node*)cache_get_ptr_ro(sector);
+	int i;
+	if (ptr->height)
+		for (i = 0; i < ptr->nkey; i++)
+			b_tree_remove(ptr->data[i].sector);
+	else
+		for (i = 0; i < ptr->nkey; i++)
+			free_map_release(ptr->data[i].sector, 1);
+	cache_release_ptr(ptr);
+	free_map_release(sector ,1);
+}
+
+
 struct inode
-  {
-    struct list_elem elem;              /* Element in inode list. */
-    block_sector_t sector;              /* Sector number of disk location. */
-    int open_cnt;                       /* Number of openers. */
-    bool removed;                       /* True if deleted, false otherwise. */
-    int deny_write_cnt;                 /* 0: writes ok, >0: deny writes. */
-    struct inode_disk data;             /* Inode content. */
-  };
-
-/* Returns the block device sector that contains byte offset POS
-   within INODE.
-   Returns -1 if INODE does not contain data for a byte at offset
-   POS. */
-static block_sector_t
-byte_to_sector (const struct inode *inode, off_t pos)
 {
-  ASSERT (inode != NULL);
-  if (pos < inode->data.length)
-    return inode->data.start + pos / BLOCK_SECTOR_SIZE;
-  else
-    return -1;
-}
+	struct list_elem elem;			/* Element in inode list. */
+	block_sector_t sector;			/* Sector number of disk location. */
+	int open_cnt;					/* Number of openers. */
+	bool removed;					/* True if deleted, false otherwise. */
+	int deny_write_cnt;				/* 0: writes ok, >0: deny writes. */
+	struct lock;					
+};
 
-/* List of open inodes, so that opening a single inode twice
-   returns the same `struct inode'. */
 static struct list open_inodes;
 
-/* Initializes the inode module. */
-void
-inode_init (void)
-{
-  list_init (&open_inodes);
+void inode_init(void){
+	list_init(&open_inodes);
 }
 
-/* Initializes an inode with LENGTH bytes of data and
-   writes the new inode to sector SECTOR on the file system
-   device.
-   Returns true if successful.
-   Returns false if memory or disk allocation fails. */
-bool
-inode_create (block_sector_t sector, off_t length)
-{
-  struct inode_disk *disk_inode = NULL;
-  bool success = false;
-
-  ASSERT (length >= 0);
-
-  /* If this assertion fails, the inode structure is not exactly
-     one sector in size, and you should fix that. */
-  ASSERT (sizeof *disk_inode == BLOCK_SECTOR_SIZE);
-
-  disk_inode = calloc (1, sizeof *disk_inode);
-  if (disk_inode != NULL)
-    {
-      size_t sectors = bytes_to_sectors (length);
-      disk_inode->length = length;
-      disk_inode->magic = INODE_MAGIC;
-      if (free_map_allocate (sectors, &disk_inode->start))
-        {
-          block_write (fs_device, sector, disk_inode);
-          if (sectors > 0)
-            {
-              static char zeros[BLOCK_SECTOR_SIZE];
-              size_t i;
-
-              for (i = 0; i < sectors; i++)
-                block_write (fs_device, disk_inode->start + i, zeros);
-            }
-          success = true;
-        }
-      free (disk_inode);
-    }
-  return success;
+bool inode_create(block_sector_t sector, off_t length){
+	struct b_tree_node* ptr = (struct b_tree_node*)cache_get_ptr_rw(sector);
+	ASSERT (length >= 0);
+	ASSERT (sizeof(struct b_tree_node) == BLOCK_SECTOR_SIZE);
+	ptr->height = ptr->nkey = 0;
+	ptr->next = ptr->parent = NULL_SECTOR;
+	ptr->magic == INODE_MAGIC;
+	cache_release_ptr(ptr);
 }
 
-/* Reads an inode from SECTOR
-   and returns a `struct inode' that contains it.
-   Returns a null pointer if memory allocation fails. */
-struct inode *
-inode_open (block_sector_t sector)
-{
-  struct list_elem *e;
-  struct inode *inode;
+struct inode* inode_open(blokc_sector_t sector){
+	struct list_elem *e;
+	struct inode *inode;
 
-  /* Check whether this inode is already open. */
-  for (e = list_begin (&open_inodes); e != list_end (&open_inodes);
-       e = list_next (e))
-    {
-      inode = list_entry (e, struct inode, elem);
-      if (inode->sector == sector)
-        {
-          inode_reopen (inode);
-          return inode;
-        }
-    }
+	for (e = list_begin(&open_inodes); e != list_end(&open_inodes); e = list_next(e)){
+		inode = list_entry(e, struct inode, elem);
+		if (inode->sector == sector){
+			inode_reopen(inode);
+			return inode;
+		}
+	}
 
-  /* Allocate memory. */
-  inode = malloc (sizeof *inode);
-  if (inode == NULL)
-    return NULL;
+	inode = malloc (sizeof(inode));
+	if (inode == NULL)
+		return NULL;
 
-  /* Initialize. */
-  list_push_front (&open_inodes, &inode->elem);
-  inode->sector = sector;
-  inode->open_cnt = 1;
-  inode->deny_write_cnt = 0;
-  inode->removed = false;
-  block_read (fs_device, inode->sector, &inode->data);
-  return inode;
+	list_push_front(&open_inodes, &inode->elem);
+	inode->sector = sector;
+	inode->open_cnt = 1;
+	inode->deny_write_cnt = 0;
+	inode->removed = false;
+	lock_init(&inode->lock);
+	return inode;
 }
 
-/* Reopens and returns INODE. */
-struct inode *
-inode_reopen (struct inode *inode)
-{
-  if (inode != NULL)
-    inode->open_cnt++;
-  return inode;
+struct inode* inode_reopen(struct inode* inode){
+	if (inode != NULL)
+		inode->open_cnt++;
+	return inode;
 }
 
-/* Returns INODE's inode number. */
-block_sector_t
-inode_get_inumber (const struct inode *inode)
-{
-  return inode->sector;
+block_sector_t inode_get_number(const struct inode* inode){
+	return inode->sector;
 }
 
-/* Closes INODE and writes it to disk.
-   If this was the last reference to INODE, frees its memory.
-   If INODE was also a removed inode, frees its blocks. */
-void
-inode_close (struct inode *inode)
-{
-  /* Ignore null pointer. */
-  if (inode == NULL)
-    return;
 
-  /* Release resources if this was the last opener. */
-  if (--inode->open_cnt == 0)
-    {
-      /* Remove from inode list and release lock. */
-      list_remove (&inode->elem);
+void inode_close(struct inode *inode){
+	if(inode==NULL)
+		return;
 
-      /* Deallocate blocks if removed. */
-      if (inode->removed)
-        {
-          free_map_release (inode->sector, 1);
-          free_map_release (inode->data.start,
-                            bytes_to_sectors (inode->data.length));
-        }
-
-      free (inode);
-    }
+	if (--inode->open_cnt == 0){
+		list_remove(&inode->elem);
+		if (inode_removed){
+			b_tree_remove(inode->sector);
+		}
+		free(inode);
+	}
 }
 
-/* Marks INODE to be deleted when it is closed by the last caller who
-   has it open. */
-void
-inode_remove (struct inode *inode)
-{
-  ASSERT (inode != NULL);
-  inode->removed = true;
+void inode_remove(struct inode *inode){
+	ASSERT(inode != NULL);
+	inode->removed = true;
 }
 
-/* Reads SIZE bytes from INODE into BUFFER, starting at position OFFSET.
-   Returns the number of bytes actually read, which may be less
-   than SIZE if an error occurs or end of file is reached. */
-off_t
-inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset)
-{
-  uint8_t *buffer = buffer_;
-  off_t bytes_read = 0;
-  uint8_t *bounce = NULL;
+off_t inode_read_at(struct inode *inode, void *buffer, off_t size, off_t offset){
 
-  while (size > 0)
-    {
-      /* Disk sector to read, starting byte offset within sector. */
-      block_sector_t sector_idx = byte_to_sector (inode, offset);
-      int sector_ofs = offset % BLOCK_SECTOR_SIZE;
-
-      /* Bytes left in inode, bytes left in sector, lesser of the two. */
-      off_t inode_left = inode_length (inode) - offset;
-      int sector_left = BLOCK_SECTOR_SIZE - sector_ofs;
-      int min_left = inode_left < sector_left ? inode_left : sector_left;
-
-      /* Number of bytes to actually copy out of this sector. */
-      int chunk_size = size < min_left ? size : min_left;
-      if (chunk_size <= 0)
-        break;
-
-      if (sector_ofs == 0 && chunk_size == BLOCK_SECTOR_SIZE)
-        {
-          /* Read full sector directly into caller's buffer. */
-          block_read (fs_device, sector_idx, buffer + bytes_read);
-        }
-      else
-        {
-          /* Read sector into bounce buffer, then partially copy
-             into caller's buffer. */
-          if (bounce == NULL)
-            {
-              bounce = malloc (BLOCK_SECTOR_SIZE);
-              if (bounce == NULL)
-                break;
-            }
-          block_read (fs_device, sector_idx, bounce);
-          memcpy (buffer + bytes_read, bounce + sector_ofs, chunk_size);
-        }
-
-      /* Advance. */
-      size -= chunk_size;
-      offset += chunk_size;
-      bytes_read += chunk_size;
-    }
-  free (bounce);
-
-  return bytes_read;
 }
 
-/* Writes SIZE bytes from BUFFER into INODE, starting at OFFSET.
-   Returns the number of bytes actually written, which may be
-   less than SIZE if end of file is reached or an error occurs.
-   (Normally a write at end of file would extend the inode, but
-   growth is not yet implemented.) */
-off_t
-inode_write_at (struct inode *inode, const void *buffer_, off_t size,
-                off_t offset)
-{
-  const uint8_t *buffer = buffer_;
-  off_t bytes_written = 0;
-  uint8_t *bounce = NULL;
+off_t inode_write_at(struct inode *inode, void *buffer, off_t size, off_t offset){
 
-  if (inode->deny_write_cnt)
-    return 0;
-
-  while (size > 0)
-    {
-      /* Sector to write, starting byte offset within sector. */
-      block_sector_t sector_idx = byte_to_sector (inode, offset);
-      int sector_ofs = offset % BLOCK_SECTOR_SIZE;
-
-      /* Bytes left in inode, bytes left in sector, lesser of the two. */
-      off_t inode_left = inode_length (inode) - offset;
-      int sector_left = BLOCK_SECTOR_SIZE - sector_ofs;
-      int min_left = inode_left < sector_left ? inode_left : sector_left;
-
-      /* Number of bytes to actually write into this sector. */
-      int chunk_size = size < min_left ? size : min_left;
-      if (chunk_size <= 0)
-        break;
-
-      if (sector_ofs == 0 && chunk_size == BLOCK_SECTOR_SIZE)
-        {
-          /* Write full sector directly to disk. */
-          block_write (fs_device, sector_idx, buffer + bytes_written);
-        }
-      else
-        {
-          /* We need a bounce buffer. */
-          if (bounce == NULL)
-            {
-              bounce = malloc (BLOCK_SECTOR_SIZE);
-              if (bounce == NULL)
-                break;
-            }
-
-          /* If the sector contains data before or after the chunk
-             we're writing, then we need to read in the sector
-             first.  Otherwise we start with a sector of all zeros. */
-          if (sector_ofs > 0 || chunk_size < sector_left)
-            block_read (fs_device, sector_idx, bounce);
-          else
-            memset (bounce, 0, BLOCK_SECTOR_SIZE);
-          memcpy (bounce + sector_ofs, buffer + bytes_written, chunk_size);
-          block_write (fs_device, sector_idx, bounce);
-        }
-
-      /* Advance. */
-      size -= chunk_size;
-      offset += chunk_size;
-      bytes_written += chunk_size;
-    }
-  free (bounce);
-
-  return bytes_written;
 }
 
-/* Disables writes to INODE.
-   May be called at most once per inode opener. */
-void
-inode_deny_write (struct inode *inode)
-{
-  inode->deny_write_cnt++;
-  ASSERT (inode->deny_write_cnt <= inode->open_cnt);
+void inode_deny_write(struct inode *inode){
+	inode->deny_write_cnt++;
+	ASSERT(inode->deny_write_cnt <= inode->open_cnt);
 }
 
-/* Re-enables writes to INODE.
-   Must be called once by each inode opener who has called
-   inode_deny_write() on the inode, before closing the inode. */
-void
-inode_allow_write (struct inode *inode)
-{
-  ASSERT (inode->deny_write_cnt > 0);
-  ASSERT (inode->deny_write_cnt <= inode->open_cnt);
-  inode->deny_write_cnt--;
+void inode_allow_write(struct inode *inode){
+	ASSERT(inode->deny_write_cnt >0);
+	ASSERT(inode->deny_write_cnt <= inode->open_cnt);
+	inode->deny_write_cnt--;
 }
 
-/* Returns the length, in bytes, of INODE's data. */
-off_t
-inode_length (const struct inode *inode)
-{
-  return inode->data.length;
+off_t inode_length(const struct inode* inode){
+	
 }
