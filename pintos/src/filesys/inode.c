826c74bf53eacd1fdf8e36aeadcc564f2a4aeaa0
yaxum62 <yaxum62@gmail.com>
2014-12-01 20:26:45 -0800

    b_tree update

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -9,13 +9,16 @@
 
 #define NULL_SECTOR -1
 
-#define INODE_MAGIC 0x494e4f44
 #define TREE_ORDER ((BLOCK_SECTOR_SIZE - 4 - 2*sizeof(block_sector_t) - sizeof(unsigned))/sizeof(struct b_tree_record))
 #define LAST_ELEM (TREE_ORDER - 1)
+
+#define LEAF_LEN (TREE_ORDER * sizeof(b_tree_record) / (sizeof(b_tree_record) + sizeof(size_t)))
+#define LEAF_LAST_ELEM (LEAF_LEN - 1)
 struct b_tree_record{
 	off_t off;
 	block_sector_t sector;
 };
+
 struct b_tree_node{
 	uint16_t height;
 	int16_t nkey;
@@ -23,17 +26,27 @@ struct b_tree_node{
 	block_sector_t parent;		// for root node, this is always NULL_SECTOR
 	unsigned magic;
 	struct b_tree_record data[TREE_ORDER];
-} ;
+};
 
-/* returns the index of the last element which <= off , or -1 if no such an element exists.*/
+struct b_tree_leaf{
+	uint16_t height;			// this will always be 0;
+	int16_t nkey;
+	block_sector_t next;
+	block_sector_t parent;
+	unsigned magic;
+	struct b_tree_record data[LEAF_LEN];
+	size_t size[LEAF_LEN];
+}
+
+// find the first element greater than off
 int16_t b_tree_search_node(struct b_tree_node* ptr, off_t off){
 	int min, max, mid;
 	min = 0; max = ptr->nkey;
 	if (min == max || ptr->data[0].off > off)
-		return -1;
+		return 0;
 	while (true){
 		if (ptr->data[min].off == off || min + 1 == max)
-			return min;
+			return min + 1;
 		mid = (min + max) / 2;
 		if (ptr->data[mid].off > off)
 			max = mid;
@@ -42,21 +55,35 @@ int16_t b_tree_search_node(struct b_tree_node* ptr, off_t off){
 	}
 }
 
-block_sector_t b_tree_find(block_sector_t root, off_t off){
-	struct b_tree_node* ptr;
 
-	ptr = (struct b_tree_node*)cache_get_ptr_ro(root);
-	if (ptr->data[0].off > off)
-		return NULL_SECTOR;
-	while (true){
-		int16_t t = b_tree_search_node(ptr, off);
-		ASSERT(t >= 0);
-		if (!ptr->height)
-			return (ptr->data[t].off == off)?ptr->data[t].sector:NULL_SECTOR;
-		root = ptr->data[t].sector;
-		cache_release_ptr(ptr);
-		ptr = (struct b_tree_node*)cache_get_ptr_ro(root);
-	}
+block_sector_t set_parent(block_sector_t sector, block_sector_t parent){
+	struct b_tree_node* ptr = (struct b_tree_node*)cache_get_ptr_rw(sector);
+	block_sector_t ret = ptr->parent;
+	ptr->parent = parent;
+	cache_release_ptr(ptr);
+	return ret;
+}
+
+block_sector_t move_child(struct b_tree_node* src, 
+						int16_t srcoff, 
+						struct b_tree_node* dst, 
+						block_sector_t dst_sector,
+						int16_t dstoff,
+						int16_t size){
+	block_sector_t ret = NULL_SECTOR;
+	int16_t i;
+	ASSERT(src->height == dst->height);
+	if (src->height == 0){
+		struct b_tree_leaf *lsrc = (struct b_tree_leaf*)src;
+		struct b_tree_leaf *ldst = (struct b_tree_leaf*)dst;
+		memmove(ldst->data + dstoff, lsrc->data + srcoff, size * sizeof(b_tree_record));
+		memmove(ldst->size + dstoff, lsrc->size + srcoff, size * sizeof(size_t));
+	}else{
+		for(i = 0 ; i < size; i++)
+			ret = set_parent(src->data[i].sector, dst_sector)
+		memmove(ldst->data + dstoff, lsrc->data + srcoff, size * sizeof(b_tree_record));
+	}	
+	return ret;
 }
 
 void b_tree_update_first(struct b_tree_node* ptr, off_t new_off){
@@ -77,170 +104,269 @@ void b_tree_update_first(struct b_tree_node* ptr, off_t new_off){
 	}
 }
 
-enum insert_result{
-	INS_SUCCESS,
-	INS_FAIL,
-	INS_FATAL
+void b_tree_node_add(struct b_tree_node* ptr, int16_t loc, off_t off, block_sector_t sector){
+	ASSERT(ptr->nkey < TREE_ORDER);
+	memmove(ptr->data + loc + 1, ptr->data + loc, (ptr->nkey - loc) * sizeof(struct b_tree_record));
+	if (loc == 0)
+		b_tree_update_first(ptr, off);
+	else
+		ptr->data[loc].off = off;
+	ptr->data[loc].sector = sector;
+	ptr->nkey++;
 }
 
-void shift_right(struct b_tree_node* ptr, int16_t loc, off_t off, block_sector_t sector){
-	if (loc == 0){
-		memmove(ptr->data + 1, ptr->data, min(LAST_ELEM, ptr->nkey)  * sizeof(struct b_tree_record));
-		b_tree_update_first(ptr, off);
-		ptr->data[0].sector = sector;
+void b_tree_leaf_add(struct b_tree_leaf *ptr, int16_t loc, off_t off, block_sector_t sector, size_t size){
+	ASSERT(ptr->nkey < LEAF_LEN)
+	b_tree_node_add((struct b_tree_node*)ptr, loc, off, sector);
+	memmove(ptr->size + loc + 1, ptr->size + loc, (ptr->nkey - loc - 1)*sizeof(size_t));
+}
+
+bool b_tree_split_root(struct b_tree_node *ptr, int16_t size, block_sector_t root_sector, int16_t *sep){
+	block_sector_t a,b;
+	struct b_tree_node *aptr, *bptr;
+
+	ASSERT(ptr->parent = NULL_SECTOR);
+	*sep = size / 2;
+	if (!free_map_allocate(1,&a))
+		return false;
+	if (!free_map_allocate(1,&b)){
+		free_map_release(a,1);
+		return false;
+	}
+	aptr = (struct b_tree_node*)cache_get_ptr_rw(a);
+	bptr = (struct b_tree_node*)cache_get_ptr_rw(b);
+
+	aptr->height = bptr->height = ptr->height;
+	aptr->nkey = *sep;
+	bptr->nkey = size - *sep;
+	aptr->next = b;
+	bptr->next = NULL_SECTOR;
+	aptr->parent = bptr->parent = root_sector;
+	move_child(ptr, 0, aptr, a, 0, aptr->nkey);
+	move_child(ptr, *sep, bptr, b, 0, bptr->nkey);
+
+	ptr->height++;
+	ptr->nkey = 2;
+	ptr->data[0].off = aptr->data[0].off;
+	ptr->data[0].sector = a;
+	ptr->data[1].sector = b;
+
+	cache_release_ptr(bptr);
+	cache_release_ptr(aptr);
+	return true;
+}
+
+bool b_tree_split_next(struct b_tree_node* ptr, int16_t size, int16_t *sep){
+	struct b_tree_node *next, *par;
+	block_sector_t next_sector, ptr_sector;
+
+	ASSERT(ptr->next == NULL_SECTOR);
+	*sep = size / 2;
+	if (!free_map_allocate(1, &next_sector))
+		return false;
+	next = (struct b_tree_node*)cache_get_ptr_rw(next_sector);
+	next->height = ptr->height;
+	next->nkey = size - *sep;
+	next->next = NULL_SECTOR;
+	next->parent = ptr->parent;
+
+	par = (struct b_tree_node*)cache_get_ptr_rw(ptr->parent);
+	ptr_sector = move_child(ptr, *sep, next, next_sector, 0, next->nkey);
+	if (!b_tree_insert_node(par,next->data[0])){
+		if (ptr_sector != NULL_SECTOR)
+			move_child(next, 0, ptr, ptr_sector, *sep, next->nkey);
+		cache_release_ptr(par);
+		cache_release_ptr(next)
+		free_map_release(next_sector, 1);
+		return false;
+	}
+
+	ptr->nkey = *sep;
+	ptr->next = next_sector;
+	cache_release_ptr(par);
+	cache_release_ptr(next);
+	return true;
+}
+
+bool b_tree_split_node(struct b_tree_node* aptr, struct b_tree_node* bptr, int16_t size, int16_t *sep1){
+	struct b_tree_node *cptr, par;
+	block_sector_t a, b, c;
+	int16_t sep2;
+
+	ASSERT(aptr->nkey == size && bptr->nkey == size);
+	*sep1 = size*2/3;
+	sep2 = size*4/3;
+	if (!free_map_allocate(1,&c))
+		return false;
+	cptr = (struct b_tree_node*)cache_get_ptr_rw(c);
+	cptr->height = ptr->height;
+	cptr->nkey = 2*size-sep2;
+	cptr->next = bptr->next;
+	cptr->parent = bptr->parent;
+
+	b = move_child(bptr, sep2-size, cptr, c, 0, cptr->nkey);
+	par = (struct b_tree_node*)cache_get_ptr_rw(cptr->parent);
+	if (!b_tree_insert_node(par, cptr->data[0].off, c, root_sector)){
+		if (b != NULL_SECTOR)
+			move_child(cptr, 0, bptr, b, sep2-size, cptr->nkey);
+		cache_release_ptr(par);
+		cache_release_ptr(cptr);
+		free_map_release(c);
+		return false;
+	}
+	move_child(bptr, 0, bptr, b, size-*sep1, sep2-size);
+	move_child(aptr, sep1, bptr, b, 0, size-*sep1);
+	aptr->nkey = *sep1;
+	bptr->nkey = sep2 - *sep1;
+	bptr->next = c;
+	cache_release_ptr(par);
+	cache_release_ptr(cptr);
+	return true;
+}
+
+bool b_tree_insert_leaf(struct b_tree_leaf* ptr,
+						off_t off,
+						block_sector_t sector,
+						size_t size,
+						block_sector_t root){
+	struct b_tree_node* nptr = (struct b_tree_node*)ptr;
+	int16_t t = b_tree_search_node(nptr, off);
+	struct b_tree_node * next = NULL;
+
+	// boundary check
+	ASSERT(t == LEAF_LEN || off+size <= ptr->data[t].off);
+	ASSERT(t == 0 || ptr->data[t-1].off + ptr->size[t-1] <= off);
+
+	if (ptr->nkey < LEAF_LEN){
+		// There are free space there!
+		b_tree_leaf_add(ptr, t, off, sector, size);
+		return true;
+	}else if (ptr->parent == NULL_SECTOR){
+		// if ptr is root node
+		int16_t sep;
+		if (!b_tree_split_root(nptr, LEAF_LEN, root, &sep))
+			return false;
+		if (t >= sep){
+			ptr = (struct b_tree_leaf*)cache_get_ptr_rw(ptr->data[1].sector);
+			t -= sep
+		}else
+			ptr = (struct b_tree_leaf*)cache_get_ptr_rw(ptr->data[0].sector);
+		b_tree_leaf_add(ptr, t, off, sector, size);
+		cache_release_ptr(ptr);
+		return true;
+	}else if (ptr->next == NULL_SECTOR){
+		// if next node is not exist.
+		int16_t sep;
+		if (!b_tree_split_next(ptr, LEAF_LEN, &sep))
+			return false;
+
+		if (t >= sep){
+			next = (struct b_tree_leaf*)cache_get_ptr_rw(ptr->next);
+			t -= sep;
+			b_tree_leaf_add(next, t, off, sector, size);
+			cache_release_ptr(next);
+		}else
+			b_tree_leaf_add(ptr, t, off, sector, size);
+		return true;
+	}else if ((next=(struct b_tree_leaf*)cache_get_ptr_rw(ptr->next))->nkey < LEAF_LEN){
+		// if exist next node and it has free space
+		if (t == LEAF_LEN)
+			b_tree_leaf_add(next, 0, off, sector, size);
+		else{
+			b_tree_leaf_add(next, 0, ptr->data[LEAF_LAST_ELEM].off, ptr->data[LEAF_LAST_ELEM].sector, ptr->size[LEAF_LAST_ELEM]);
+			ptr->nkey--;
+			b_tree_leaf_add(ptr, t, off, sector, size);
+		}
+		cache_release_ptr(next);
+		return true;
 	}else{
-		memmove(ptr->data + loc + 1, ptr->data + loc, (min(LAST_ELEM, ptr->nkey) - loc) * sizeof(struct b_tree_record))
-		ptr->data[loc].off = off;
-		ptr->data[loc].sector = sector;
+		// both this and next node are full.
+		int16_t sep;
+		if (!b_tree_split_node(nptr, next, LEAF_LEN, &sep)){
+			cache_release_ptr(next);
+			return false;
+		}
+		if (t >= sep){
+			t-=sep;
+			b_tree_leaf_add(next, t, off ,sector,size);
+		}else
+			b_tree_leaf_add(ptr, t, off, sector, size);
+		cache_release_ptr(next);
+		return true;
 	}
 }
 
-inline void set_parent(block_sector_t parent, block_sector_t child){
-	struct b_tree_node* childptr = (struct b_tree_node*)cache_get_ptr_rw(child);
-	childptr->parent = parent;
-	cache_release_ptr(childptr);
-}
-
-inline void move_child(struct b_tree_block* src,
-						int16_t src_off,
-						struct b_tree_block* dst,
-						block_sector_t dst_sector.
-						int16_t dst_off,
-						int16_t size){
-	int i;
-	ASSERT(src->height == dst->height);
-	if (src != dst && src->height)
-		for (i = 0; i < size; i++)
-			set_parent(dst_sector, src->data[src_off + i].sector);
-	memmove(dst->data+dst_off, src->data+src_off, size * sizeof(struct b_tree_record));
-}
-
-bool b_tree_insert(struct b_tree_node* ptr, off_t off, block_sector_t sector, block_sector_t root){
-	int16_t t = b_tree_search_node(ptr, off);
+bool b_tree_insert_node(struct b_tree_node* ptr, 
+				off_t off, 
+				block_sector_t sector, 
+				block_sector_t root){
+	int16_t t = b_tree_search_node(ptr, off) + 1;
 	struct b_tree_node * next = NULL;
 
-	ASSERT(t < 0 || ptr->offs[t] != off);
+	ASSERT(t == 0 || ptr->offs[t-1] != off);
 
 	if (ptr->nkey < TREE_ORDER){
-		shift_right(ptr, t + 1, off, sector);
-		ptr->nkey++;
+		// There are free space there!
+		b_tree_node_add(ptr, t, off, sector);
 		return true;
 	}else if (ptr->parent == NULL_SECTOR){
-		// It's root node
-		bool res;
-		block_sector_t a,b;
-		struct b_tree_node *aptr, *bptr;
-		int16_t sep = TREE_ORDER / 2;
-		if (!free_map_allocate(1, &a))
+		// if ptr is root node
+		int16_t sep;
+		block_sector_t newpar_sector;
+		if (!b_tree_split_root(ptr, TREE_ORDER, root, &sep))
 			return false;
-		if (!free_map_allocate(1, &b)){
-			free_map_release(a, 1);
-			return false;
-		}
-		aptr = (struct b_tree_node*)cache_get_ptr_rw(a);
-		bptr = (struct b_tree_node*)cache_get_ptr_rw(b);
-
-		aptr->height = bptr->height = ptr->height;
-		aptr->nkey = sep;
-		bptr->nkey = TREE_ORDER - sep;
-		aptr->next = b;
-		bptr->next = NULL_SECTOR;
-		aptr->parent = bptr->parent = root;
-		move_child(ptr, 0, aptr, a, 0, aptr->nkey);
-		move_child(ptr, sep, bptr, b, 0, bptr->nkey);
-
-		ptr->height++;
-		ptr->nkey = 2;
-		ptr->data[0].off = aptr->data[0].off;
-		ptr->data[0].sector = a;
-		ptr->data[1].off = bptr->data[0].off;
-		ptr->data[1].sector = b;
-		
-		// TODO res =  retry in aptr and bptr
-		cache_release_ptr(bptr);
-		cache_release_ptr(aptr);
-		return res;
+		if (t >= sep){
+			newpar_sector = ptr->data[1].sector;
+			t -= sep;
+		}else
+			newpar_sector = ptr->data[0].sector;
+		ptr = (struct b_tree_node*)cache_get_ptr_rw(newpar_sector);
+		set_parent(sector, newpar_sector);
+		b_tree_node_add(ptr, t, off, sector);
+		cache_release_ptr(ptr);
+		return true;
 	}else if(ptr->next == NULL_SECTOR){
 		// if next node is not exist.
-		bool res;
-		struct b_tree_node *next, *par;
-		block_sector_t next_sector;
-		int16_t sep = TREE_ORDER / 2;
-		if (!free_map_allocate(1, &next_sector)
+		int16_t sep;
+		if (!b_tree_split_next(ptr, TREE_ORDER, &sep))
 			return false;
-		next = (struct b_tree_node*)cache_get_ptr_rw(ptr->next);
-		next->height = ptr->height;
-		next->nkey = TREE_ORDER - sep;
-		next->next = NULL_SECTOR;
-		next->parent = ptr->parent;
-
-		par = (struct b_tree_node*)cache_get_ptr_rw(ptr->parent);
-		move_child(ptr, sep, next, ptr->next, 0, next->nkey);
-		if (!b_tree_insert(par, next->data[0].off, ptr->next, root)){
-			cache_release_ptr(par);
+		if (t >= sep){
+			next = (struct b_tree_node*)cache_get_ptr_rw(ptr->next);
+			t-=sep;
+			set_parent(sector, ptr->next);
+			b_tree_node_add(next, t, off, sector);
 			cache_release_ptr(next);
-			free_map_release(next_sector);
-			return false;
-		}
-		ptr->nkey = sep;
-		ptr->next = next_sector;
-		// TODO res = retry in ptr and next
-		cache_release_ptr(par);
-		cache_release_ptr(next);
-		return res;
+		}else
+			b_tree_node_add(ptr, t, off, sector);
+		return true;
 	}else if ((next=(struct b_tree_node*) cache_get_ptr_rw(ptr->next))->nkey < TREE_ORDER){
-		// if exist next node and it's not full.
-		if (loc == TREE_ORDER){
-			shift_right(next, 0, off, record);
-			if (ptr->height)
-				set_parent(ptr->next, sector);
+		// if exist next node and it has free space
+		if (t == TREE_ORDER){
+			set_parent(sector, ptr->next);
+			b_tree_node_add(next, 0, off, sector);
 		}else{
-			shift_right(next, 0, ptr->data[LAST_ELEM].off, ptr->data[LAST_ELEM].sector);
-			next->data[0] = ptr->data[LAST_ELEM];
-			if (ptr->height)
-				set_parent(ptr->next, next->data[0].sector)
-			shift_right(ptr, loc, off, sector);
+			struct b_tree_record *swap = ptr->data + LAST_ELEM;
+			set_parent(swap->sector, ptr->next);
+			b_tree_node_add(next, 0, swap->off, swap->sector);
+			ptr->nkey--;
+			b_tree_node_add(ptr, t, off, sector);
 		}
-		next->nkey++;
 		cache_release_ptr(next);
 		return true;
 	}else{
 		// both this and next node is full.
-		bool res;
-		off_t old_off;
-		struct b_tree_node *mid, par;
-		block_sector_t mid_sector;
-		int16_t sep1 = TREE_ORDER *2 / 3, sep2 = TREE_ORDER * 4 / 3;
-		if (!free_map_allocate(1, &mid_sector))
-			return false;
-		mid = (struct b_tree_node*)cache_get_ptr_rw(mid_sector);
-		mid->height = ptr->height;
-		mid->nkey = sep2 - sep1;
-		mid->next = ptr->next;
-		mid->parent = ptr->parent;
-
-		move_child(ptr, sep1, mid, mid_sector, 0,TREE_ORDER-sep1);
-		move_child(next, 0, mid, mid_sector, TREE_ORDER-sep1, sep2 - TREE_ORDER);
-		par = (struct b_tree_node*)cache_get_ptr_rw(ptr->parent);
-		if (!b_tree_insert(par, mid->data[0].off, mid_sector, root)){
-			cache_release_ptr(par);
-			cache_release_ptr(mid);
+		int16_t sep;
+		if (!b_tree_split_node(ptr, next, TREE_ORDER, &sep)){
 			cache_release_ptr(next);
-			free_map_allocate(mid_sector, 1);
 			return false;
 		}
-		ptr->nkey = sep1;
-		ptr->next = mid_sector;
-
-		next->nkey =  2*TREE_ORDER - sep2;
-		next->data[0].off;
-		b_tree_update_first(next, next->data[sep2-TREE_ORDER].off);
-		move_child(next, sep2-TREE_ORDER, next, mid->next, 0, next->nkey);
-		// TODO res = retry in ptr, mid and next
-		cache_release_ptr(par);
-		cache_release_ptr(mid);
+		if (t >= sep){
+			t -= sep;
+			b_tree_node_add(next, t, off, sector);
+		}else
+			b_tree_node_add(ptr, t, off ,sector);
 		cache_release_ptr(next);
-		return res;
+		return true;
 	}
 }
 
@@ -248,15 +374,14 @@ void b_tree_remove(block_sector_t sector){
 	struct b_tree_node* ptr = (struct b_tree_node*)cache_get_ptr_ro(sector);
 	int i;
 	if (ptr->height)
-		for (i = 0; i < ptr->nkey; i++)
+		for(i = 0; i < ptr->nkey; i++)
 			b_tree_remove(ptr->data[i].sector);
 	else
-		for (i = 0; i < ptr->nkey; i++)
-			free_map_release(ptr->data[i].sector, 1);
+		for(i = 0; i < ptr->nkey; i++)
+			free_map_release(ptr->data[i].sector, ptr->size[i])
 	cache_release_ptr(ptr);
-	free_map_release(sector ,1);
-}
-
+	free_map_release(sector, 1);
+}}
 
 struct inode
 {
@@ -275,7 +400,7 @@ void inode_init(void){
 }
 
 bool inode_create(block_sector_t sector, off_t length){
-	struct b_tree_node* ptr = (struct b_tree_node*)cache_get_ptr_rw(sector);
+	struct b_tree_node* ptr = (struct b_tree_leaf*)cache_get_ptr_rw(sector);
 	ASSERT (length >= 0);
 	ASSERT (sizeof(struct b_tree_node) == BLOCK_SECTOR_SIZE);
 	ptr->height = ptr->nkey = 0;
@@ -319,7 +444,6 @@ block_sector_t inode_get_number(const struct inode* inode){
 	return inode->sector;
 }
 
-
 void inode_close(struct inode *inode){
 	if(inode==NULL)
 		return;
@@ -340,23 +464,4 @@ void inode_remove(struct inode *inode){
 
 off_t inode_read_at(struct inode *inode, void *buffer, off_t size, off_t offset){
 
-}
-
-off_t inode_write_at(struct inode *inode, void *buffer, off_t size, off_t offset){
-
-}
-
-void inode_deny_write(struct inode *inode){
-	inode->deny_write_cnt++;
-	ASSERT(inode->deny_write_cnt <= inode->open_cnt);
-}
-
-void inode_allow_write(struct inode *inode){
-	ASSERT(inode->deny_write_cnt >0);
-	ASSERT(inode->deny_write_cnt <= inode->open_cnt);
-	inode->deny_write_cnt--;
-}
-
-off_t inode_length(const struct inode* inode){
-	
 }
